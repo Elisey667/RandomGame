d086debdedfc9d840f952f31affe19a3e69b72ef
Elisey667 <sergejzidov667@gmail.com>
2025-11-06 18:01:39 +0300

    Update main.cpp
    Исправил недочёты в коде

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <thread>
 #include <random>
 
 using namespace std;
 
 int main() {
-    std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<int> dist(1, 100);
+ random_device rd;
+ mt19937 gen(rd());
+ uniform_int_distribution<int> dist(1, 100);
 int input_number;
 int Number = dist(gen);
 while (true) {
